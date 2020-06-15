4eed0153e0da19dce931854d9389dbbd7a9e5c28
Bart Krawczyk <ZITAdmin@BART.pkmtychy.local>
2020-06-15 13:32:15 +0200

    First commit

---
+++ src/main/java/pl/programodawca/liner/LinerApplication.java
@@ -0,0 +1,13 @@
+package pl.programodawca.liner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinerApplication.class, args);
+    }
+
+}
